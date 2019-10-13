c0f58554d07d9d15c4fdaa8c8d2597392e6abbd3
lukasz-ciupek <lukasz.ciupek@o2.pl>
2019-10-13 15:56:16 +0200

    Adding test checking the addBook method

--- src/test/java/pl/sda/AppTest.java
+++ src/test/java/pl/sda/AppTest.java
@@ -6,14 +6,21 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import pl.sda.library.AppConfig;
+import pl.sda.library.model.Book;
 import pl.sda.library.repository.BookRepository;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 /**
  * Unit test for simple App.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {AppConfig.class})
 public class AppTest
 {
     /**
@@ -23,8 +30,33 @@ public class AppTest
     private BookRepository bookRepository;
 
     @Test
-    public void shouldAnswerWithTrue()
+    public void shouldReturnTrueAfterAddBook()
     {
-        assertTrue( true );
+        //given
+        String title = "test";
+        String author = "ja";
+
+        //when
+        bookRepository.addBook(title,author);
+        Integer actualSize = bookRepository.getBooks().size();
+
+        String titleFromRepository = bookRepository.getBooks().stream()
+                .reduce((first, second) -> second).get().getTitle();
+        String authorFromRepository = bookRepository.getBooks().stream()
+                .reduce((first, second) -> second).get().getAuthor();
+        Integer idFromRepository = bookRepository.getBooks().stream()
+                .reduce((first, second) -> second).get().getId();
+        LocalDate borrowedTillFromRepository = bookRepository.getBooks().stream()
+                .reduce((first, second) -> second).get().getBorrowedTill();
+
+        //then
+        Assert.assertEquals(actualSize,11,0.0000001);
+        Assert.assertEquals(titleFromRepository,"test");
+        Assert.assertEquals(authorFromRepository,"ja");
+        Assert.assertEquals(idFromRepository,11,0.0000001);
+        Assert.assertNull(borrowedTillFromRepository);
+
     }
+
+ 
 }
